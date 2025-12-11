407c8d492da326417da617b0133272374669f5de
sher2od <sherzodqarshiboyev543@gmail.com>
2025-12-11 22:36:04 +0300

    db

--- core/settings.py
+++ core/settings.py
@@ -81,11 +81,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'NAME': os.getenv('DB_NAME'),
     }
 }
-
+    
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
